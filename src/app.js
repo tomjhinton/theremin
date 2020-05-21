2af51daee529709a4d6fe47a7ae619678cb7f13c
Tom Hinton <tomjhinton@gmail.com>
2020-05-21 19:13:38 +0100

    three setup

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
 const THREE = require('three')
 import Tone from 'tone'
 import * as posenet from '@tensorflow-models/posenet'
+
+let  scene = new THREE.Scene()
+let  camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 0.5, 10000 )
+
+scene.add(camera)
