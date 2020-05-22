35ed459db61cf7aaf8b9e440cd2e9d5727d01e6f
Tom Hinton <tomjhinton@gmail.com>
2020-05-22 21:18:28 +0100

    renderer light added

--- src/app.js
+++ src/app.js
@@ -6,3 +6,21 @@ let  scene = new THREE.Scene()
 let  camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 0.5, 10000 )
 
 scene.add(camera)
+
+const light = new THREE.DirectionalLight( 0xffffff, 1.75 )
+var d = 5
+
+light.position.set( d, d, d )
+
+light.castShadow = true
+//light.shadowCameraVisible = true;
+
+const theremin = document.getElementById('theremin')
+
+  const renderer = new THREE.WebGLRenderer()
+  renderer.setSize( window.innerWidth, window.innerHeight )
+  theremin.appendChild( renderer.domElement )
+
+scene.add( light )
+
+scene.background = new THREE.Color( 0x000000 )
